6074148cf69605e12df02c05494db52d231ea43a
MrRamoun <omartarekramoun@gmail.com>
2022-03-20 01:30:35 +0200

    Add missing import statement

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
