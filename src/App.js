6fbcdc5ec7c365388a1c7e87905d5e4aaf941160
MrRamoun <omartarekramoun@gmail.com>
2022-03-20 05:02:11 +0200

    Fix conflict

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router, Navigate, Route, Routes,
+} from 'react-router-dom';
+import './App.css';
+import Details from './components/Details';
+import Home from './components/Home';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+const App = () => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:country" element={<Details />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
@@ -12,9 +10,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename={process.env.PUBLIC_URL}>
-        <App />
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
