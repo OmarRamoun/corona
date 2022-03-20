606f149ed426a964c76825b41356857450639413
MrRamoun <omartarekramoun@gmail.com>
2022-03-20 04:50:51 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
-import {
-  BrowserRouter as Router, Navigate, Route, Routes,
-} from 'react-router-dom';
-import './App.css';
-import Details from './components/Details';
-import Home from './components/Home';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:country" element={<Details />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  </Router>
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
