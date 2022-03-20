82941ae73998784500a87a53dc06f5824ed1e18f
MrRamoun <omartarekramoun@gmail.com>
2022-03-20 04:56:29 +0200

    Fix linters erros

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
